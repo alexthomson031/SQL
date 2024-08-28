e8726358535220415672d4f5e944b67ab7942b6f
Netotest <athomson@mail.ru>
2024-08-28 09:05:04 +0300

    Com1

---
+++ src/test/java/ru/netology/data/SqlData.java
@@ -0,0 +1,106 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import lombok.val;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SqlData {
+
+    public static String getVerificationCode() {
+        val selectSQL = "SELECT code FROM auth_codes WHERE created = (select MAX(created) FROM auth_codes);";
+        val runner = new QueryRunner();
+        String code = null;
+
+        try (
+                val conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/app", "app", "pass"
+                );
+        ) {
+            code = runner.query(conn, selectSQL, new ScalarHandler<>());
+        } catch (SQLException throwables) {
+        }
+        return code;
+    }
+
+    public static void cleanData() {
+        val runner = new QueryRunner();
+        val cleanUsers = "DELETE FROM users;";
+        val cleanCards = "DELETE FROM cards;";
+        val cleanAuth_Codes = "DELETE FROM auth_codes;";
+        val cleanCard_Transactions = "DELETE FROM card_transactions;";
+        try (
+                val conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/app", "app", "pass"
+                );
+        ) {
+            runner.execute(conn, cleanCards);
+            runner.execute(conn, cleanAuth_Codes);
+            runner.execute(conn, cleanUsers);
+            runner.execute(conn, cleanCard_Transactions);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static String getUserStatus(String login) {
+        val selectSQL = "SELECT status FROM users WHERE login = ?;";
+        val runner = new QueryRunner();
+        String status = null;
+
+        try (
+                val conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/app", "app", "pass"
+
+                );
+        ) {
+            status = runner.query(conn, selectSQL, new ScalarHandler<>(), login);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return status;
+    }
+
+    public static void setNewUser() {
+        val faker = new Faker();
+        val runner = new QueryRunner();
+        val dataSQL = "INSERT INTO users(id, login, password) VALUES (?, ?, ?);";
+        val cleanUsers = "DELETE FROM users WHERE id = '1';";
+        val cleanAuth_Codes = "DELETE FROM auth_codes;";
+
+        try (
+                val conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/app", "app", "pass"
+                );
+
+        ) {
+            runner.execute(conn, cleanAuth_Codes);
+            runner.execute(conn, cleanUsers);
+            runner.update(conn, dataSQL, 1, faker.name().username(), "$2a$10$LupomVF1pCYXveW2SuzwYuk6kTEuEr7lyhYq8jdv7TdLrIzhNmFd.");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static String getNewUser() {
+        setNewUser();
+        String user = null;
+        val runner = new QueryRunner();
+        val dataSQL = "SELECT login FROM users WHERE id = '1';";
+
+        try (
+                val conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/app", "app", "pass"
+                );
+        ) {
+            user = runner.query(conn, dataSQL, new ScalarHandler<>());
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return user;
+    }
+}
---
+++ src/test/java/ru/netology/test/LoginTest.java
@@ -0,0 +1,58 @@
+package ru.netology.test;
+
+import lombok.val;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SqlData;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginTest {
+    private LoginPage loginPage;
+
+    @BeforeEach
+    void setup() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    void loginWithExistUser() {
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = SqlData.getVerificationCode();
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void addNewUserAndLogin() {
+        val authInfo = DataHelper.getUser();
+        val verificationPage = loginPage.validLogin(authInfo);
+        sleep(900);
+        val verificationCode = SqlData.getVerificationCode();
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void blockIfLoginWithWrongPasswordThreeTime() {
+        val authInfo = DataHelper.getUserWithWrongPassword();
+        loginPage.validLogin(authInfo);
+        loginPage.errorMessage();
+        loginPage.clearFields();
+        loginPage.validLogin(authInfo);
+        loginPage.errorMessage();
+        loginPage.clearFields();
+        loginPage.validLogin(authInfo);
+        val status = SqlData.getUserStatus(authInfo.getLogin());
+        assertEquals("blocked", status);
+    }
+
+    @AfterAll
+    static void cleanTables() {
+        SqlData.cleanData();
+    }
+}
